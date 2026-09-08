16090c4b908a477681686735cda4a00128117ffd
guaurritas-star <guaurritas@gmail.com>
2026-09-07 20:15:21 -0600

    Add SkydropX shipping plugin config

---
+++ docs/wix-skydropx-direct/skydropxDirect-config.js
@@ -0,0 +1,7 @@
+export function getConfig() {
+  return {
+    name: 'Guaurritas · SkydropX Direct',
+    description:
+      'Cotización nacional en tiempo real usando la API oficial de SkydropX.',
+  };
+}
